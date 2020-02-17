eb8abb003c8a7a40816f22b1f2f20a55fe9f151b
kumarsuraj9450 <kumarsuraj9450@gmail.com>
2020-02-17 10:54:24 +0530

    models created

--- mlApp/classification/models.py
+++ mlApp/classification/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class dimensions(models.Model):
+    petalWidth  =models.DecimalField(decimal_places=2,max_digits=4,blank=False,null=False)
+    petalLength =models.DecimalField(decimal_places=2,max_digits=4,blank=False,null=False)
+    sepalWidth  =models.DecimalField(decimal_places=2,max_digits=4,blank=False,null=False)
+    sepalLength =models.DecimalField(decimal_places=2,max_digits=4,blank=False,null=False)
